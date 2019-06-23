11a08422eea03b64690b53152ac5142eca00ace7
weimuhua <weimuhua0812@126.com>
2019-06-23 20:01:04 +0800

    test user ListView.Builder

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,19 +9,27 @@ class MyAppPage extends StatefulWidget {
 }
 
 class _SampleAppPageState extends State<MyAppPage> {
+  List widgets = <Widget>[];
+
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < 51; i++) {
+      widgets.add(getRow(i));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Sample App"),
       ),
-      body: ListView(
-        children: <Widget>[
-          Text('List Row One', style: new TextStyle(fontSize: 20)),
-          Text('List Row Two', style: new TextStyle(fontSize: 20)),
-          Text('List Row Three', style: new TextStyle(fontSize: 20)),
-          Text('List Row Four', style: new TextStyle(fontSize: 20)),
-        ],
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int pos) {
+          return getRow(pos);
+        },
+        itemCount: widgets.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -30,6 +38,20 @@ class _SampleAppPageState extends State<MyAppPage> {
         tooltip: 'jump to another page',
         child: Icon(Icons.navigate_next),
       ),
+    );
+  }
+
+  Widget getRow(int i) {
+    return GestureDetector(
+      child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Text("Row $i", style: TextStyle(fontSize: 18))),
+      onTap: () {
+        setState(() {
+          widgets.add(getRow(widgets.length + 1));
+          print('row $i');
+        });
+      },
     );
   }
 }
