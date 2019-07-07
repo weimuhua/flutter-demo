c1c0054bc0520b6beccf039f4bc4f80be42baa52
weimuhua <weimuhua0812@126.com>
2019-07-07 19:54:32 +0800

    取出无用的Center Widget.

--- lib/page/stack_page.dart
+++ lib/page/stack_page.dart
@@ -7,46 +7,44 @@ class StackPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Container(
       color: Color(0xFF080324),
-      child: new Center(
-        child: new Stack(
-          children: <Widget>[
-            new Align(
-              alignment: FractionalOffset.center,
-              heightFactor: 40.0,
-              widthFactor: 40.0,
-              child: new Image.network(
-                //加载网络图片
-                'https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/'
-                'c0%3Dbaike150%2C5%2C5%2C150%2C50/sign=92b9bfe58ed6277ffd1f3a6a49517455/4e4a20a4462309f785ea864c7b0e0cf3d6cad6e2.jpg',
-                height: 300.0,
-                width: 300.0,
+      child: new Stack(
+        children: <Widget>[
+          new Align(
+            alignment: FractionalOffset.center,
+            heightFactor: 40.0,
+            widthFactor: 40.0,
+            child: new Image.network(
+              //加载网络图片
+              'https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/'
+              'c0%3Dbaike150%2C5%2C5%2C150%2C50/sign=92b9bfe58ed6277ffd1f3a6a49517455/4e4a20a4462309f785ea864c7b0e0cf3d6cad6e2.jpg',
+              height: 300.0,
+              width: 300.0,
+            ),
+          ),
+          new BackgroundWidget(),
+          Container(
+            alignment: FractionalOffset.bottomLeft,
+            child: new Opacity(
+              opacity: 0.5, //不透明度
+              child: new Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.blue,
               ),
             ),
-            new BackgroundWidget(),
-            Container(
-              alignment: FractionalOffset.bottomLeft,
-              child: new Opacity(
-                opacity: 0.5, //不透明度
-                child: new Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.blue,
-                ),
+          ),
+          Container(
+            alignment: FractionalOffset.bottomRight,
+            child: new Opacity(
+              opacity: 0.5,
+              child: new Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.red,
               ),
             ),
-            Container(
-              alignment: FractionalOffset.bottomRight,
-              child: new Opacity(
-                opacity: 0.5,
-                child: new Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.red,
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
